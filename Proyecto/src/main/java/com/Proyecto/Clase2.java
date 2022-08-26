755a6dd9ba88fab6ec681fa0d901cacd86e68f5a
Maquina Virtual <maquinavirtual@gmail.com>
2022-08-26 01:44:08 -0500

    clase 2

---
+++ Proyecto/src/main/java/com/Proyecto/Clase2.java
@@ -0,0 +1,38 @@
+package com.Proyecto;
+
+public class Clase2 {
+
+    private String atributo5;
+    private int atributo6;
+
+    public Clase2(String atributo5, int atributo6) {
+        this.atributo5 = atributo5;
+        this.atributo6 = atributo6;
+    }
+
+    public String getAtributo5() {
+        return atributo5;
+    }
+
+    public void setAtributo5(String atributo5) {
+        this.atributo5 = atributo5;
+    }
+
+    public int getAtributo6() {
+        return atributo6;
+    }
+
+    public void setAtributo6(int atributo6) {
+        this.atributo6 = atributo6;
+    }
+
+    public int monto(){
+        return -1;
+
+    }
+
+    public String usuario(){
+        return "-1";
+    }
+
+}
